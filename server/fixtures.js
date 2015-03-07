1ee23f8e991b5f1a0250cef9463d5605c0506fa3
aspin <tsunami70875@gmail.com>
2015-03-06 23:40:35 -0600

    finished up admin interface

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,5 +1,3 @@
-var DEBUG = 1;
-
 if (Meteor.users.find().count() === 0) {
     var admin = Assets.getText('admin').split(',')
 
@@ -14,26 +12,26 @@ if (Meteor.users.find().count() === 0) {
     });
 }
 
-if (Teams.find().count() === 0 && DEBUG === 1) {
-    var teams = [
-        {
-            name: "Kevin's Crew",
-            members: ['kevin', 'jason'],
-            score: 5000
-        },
-        {
-            name: "Brittany's Team",
-            members: ['britt', 'lee'],
-            score: 1000
-        },
-        {
-            name: "noob_team",
-            members: ['noob1', 'noob2'],
-            score: 100
-        }
-    ];
+// if (Teams.find().count() === 0 && DEBUG === 1) {
+//     var teams = [
+//         {
+//             name: "Kevin's Crew",
+//             members: ['kevin', 'jason'],
+//             score: 5000
+//         },
+//         {
+//             name: "Brittany's Team",
+//             members: ['britt', 'lee'],
+//             score: 1000
+//         },
+//         {
+//             name: "noob_team",
+//             members: ['noob1', 'noob2'],
+//             score: 100
+//         }
+//     ];
 
-    for(var i in teams) {
-        Teams.insert(teams[i]);
-    }
-}
+//     for(var i in teams) {
+//         Teams.insert(teams[i]);
+//     }
+// }
